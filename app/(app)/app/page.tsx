a4011a1191278c9b9649183175f293cec1ebb0ae
Said zain <saidzain897@gmail.com>
2024-03-01 12:25:06 +0300

    Add fetch all posts functionality

--- app/(app)/app/page.tsx
+++ app/(app)/app/page.tsx
@@ -13,5 +13,38 @@ export default async function AppPage() {
     return redirect("/");
   }
 
+  const fetchAllPosts = async () => {
+    const { data, error } = await supabase.from("posts").select();
+    console.log(data, error);
+  };
+  const insertPost = async (
+    course_name: string,
+    course_section: string,
+    course_days: string,
+    course_hours: string,
+    exchange_for: string
+  ) => {
+    // select user id using email
+    const { data: userId } = await supabase
+      .from("users")
+      .select("id")
+      .eq("email", user.email);
+
+    const { data, error } = await supabase
+      .from("posts")
+      .insert([
+        {
+          course_name,
+          course_section,
+          course_days,
+          course_hours,
+          exchange_for,
+          user_id: userId ? userId[0].id : null,
+        },
+      ])
+      .select();
+    console.log(data, error);
+  };
+
   return <div className="">app</div>;
 }

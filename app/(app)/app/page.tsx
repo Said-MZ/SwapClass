715fdcefb9743d9be111bc077987eba230970911
Said zain <saidzain897@gmail.com>
2024-03-02 01:07:00 +0300

    Move skeletons to seperate file

--- app/(app)/app/page.tsx
+++ app/(app)/app/page.tsx
@@ -1,3 +1,4 @@
+import { CardsSkeleton } from "@/components/skeletons/CardSkeleton";
 import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 import { redirect } from "next/navigation";
@@ -62,7 +63,7 @@ export default async function AppPage() {
       <h1 className="text-xl lg:text-3xl font-bold !leading-tight mx-auto max-w-[920px] text-center mb-12">
         Explore students' posts
       </h1>
-      <Suspense fallback={<Skeleton />}>
+      <Suspense fallback={<CardsSkeleton />}>
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {posts ? (
             posts.map(async (post: any) => {
@@ -125,47 +126,4 @@ export default async function AppPage() {
   );
 }
 
-const CardSkeleton = () => {
-  return (
-    <div className="flex flex-col gap-2  backdrop-blur-[1px] p-4 border-2 rounded-md">
-      <div>
-        <span className="animate-pulse bg-neutral-300 block h-4 w-20 rounded-md my-2"></span>
 
-        <span className="animate-pulse bg-neutral-600 block h-4 w-64 rounded-md"></span>
-      </div>
-      <div className=" w-full h-[1px] bg-neutral-800 mb-2"></div>
-      <h3 className="capitalize text-xl font-bold">
-        <span className="animate-pulse bg-neutral-300 block h-4 w-20 rounded-md my-2"></span>
-      </h3>
-      <ul className="flex flex-col gap-4">
-        <li className="flex gap-2">
-          <span className="animate-pulse bg-neutral-300 block h-4 w-28 rounded-md"></span>
-          <span className="animate-pulse bg-neutral-600 block h-4 w-full rounded-md"></span>
-        </li>
-        <li className="flex gap-2">
-          <span className="animate-pulse bg-neutral-300 block h-4 w-28 rounded-md"></span>
-          <span className="animate-pulse bg-neutral-600 block h-4 w-full rounded-md"></span>
-        </li>
-        <li className="flex gap-2">
-          <span className="animate-pulse bg-neutral-300 block h-4 w-28 rounded-md"></span>
-          <span className="animate-pulse bg-neutral-600 block h-4 w-full rounded-md"></span>
-        </li>
-      </ul>
-
-      <span className="animate-pulse bg-neutral-300 block h-4 w-28 rounded-md mt-4"></span>
-      <span className="animate-pulse bg-neutral-600 block h-4 w-full rounded-md"></span>
-      <span className="animate-pulse bg-neutral-600 block h-4 w-full rounded-md"></span>
-    </div>
-  );
-};
-
-const Skeleton = () => {
-  const arr = Array.from({ length: 8 }, (_, i) => i);
-  return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      {arr.map((_, i) => (
-        <CardSkeleton key={i} />
-      ))}
-    </div>
-  );
-};

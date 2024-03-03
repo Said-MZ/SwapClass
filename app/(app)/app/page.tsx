b17dee784af94d0605f9de8b6fc9fc62a6daa504
Said zain <saidzain897@gmail.com>
2024-03-03 20:56:16 +0300

    organize posts page

--- app/(app)/app/page.tsx
+++ app/(app)/app/page.tsx
@@ -32,7 +32,6 @@ export default async function AppPage() {
         />
       </div>
 
-      {/* divider with text or */}
       <div className="flex justify-center items-center mb-10">
         <div className="w-full h-[1px] bg-neutral-600 mr-3"></div>
         OR
@@ -49,6 +48,11 @@ export default async function AppPage() {
           {posts ? (
             posts.map(async (post: any) => {
               const user = await getUserById(post.user_id);
+              const date = new Date(post.created_at)
+                .toISOString()
+                .split("T")[0];
+              const name = user ? user[0]?.name : "Anonymous";
+              const email = user ? user[0].email : "";
 
               return (
                 <Link
@@ -57,16 +61,13 @@ export default async function AppPage() {
                   className="flex flex-col gap-2  backdrop-blur-[1px] p-4 border-2 rounded-md sm:hover:brightness-125 sm:hover:-translate-y-1 sm:hover:border-neutral-600 transition-all duration-200"
                 >
                   <div>
-                    <h2 className="capitalize text-lg font-bold">
-                      {(user && user[0].name) || "Anonymous"}
-                    </h2>
+                    <h2 className="capitalize text-lg font-bold">{name}</h2>
                     <p className="text-xs text-neutral-400">
-                      {user && user[0].email}{" "}
+                      {email}{" "}
                       <span className="font-extrabold text-neutral-300">
                         &nbsp;•&nbsp;
                       </span>{" "}
-                      {post &&
-                        new Date(post.created_at).toISOString().split("T")[0]}
+                      {date}
                     </p>
                   </div>
                   <div className=" w-full h-[1px] bg-neutral-800 mb-2"></div>

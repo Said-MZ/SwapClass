64bf59ebe92d40561de34b664b937760c72ed4c4
Said zain <saidzain897@gmail.com>
2024-03-07 21:55:25 +0300

    Add how it works section

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -4,6 +4,20 @@ const LandingPage = async () => {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <Hero />
+      <h2 className="text-xl lg:text-3xl font-bold !leading-tight mx-auto max-w-[920px] text-center mb-6">
+        How it works
+      </h2>
+      <iframe
+        // glow around the video
+        className="aspect-video max-w-[90vw] max-h-96 sm:max-w-screen-sm sm:max-h-96 rounded-md border-2"
+        width="1676"
+        height="766"
+        src="https://www.youtube.com/embed/x2Ry1oPmSpA"
+        title="How to use swap class"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 };
